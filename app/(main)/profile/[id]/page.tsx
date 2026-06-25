eda2a396a77e3ad4fc06b2c136e4ea56903ca97e
Bastienldx <bastien.ledoux@viacesi.fr>
2026-06-25 21:05:22 +0200

    fix: use client in pages

--- app/(main)/profile/[id]/page.tsx
+++ app/(main)/profile/[id]/page.tsx
@@ -1,17 +1,17 @@
-"use client";
-
 import React from "react";
-import { useParams } from "next/navigation";
 import { PhoneFrame } from "@/components/layout/PhoneFrame";
 import { ProfileContainer } from "@/components/ProfileContainer";
 
-export default function OtherProfilePage() {
-  const params = useParams();
-  const targetId = params.id as string;
+export default async function OtherProfilePage({ 
+  params 
+}: { 
+  params: Promise<{ id: string }> 
+}) {
+  const { id } = await params;
 
   return (
     <PhoneFrame>
-      <ProfileContainer targetUserId={targetId} />
+      <ProfileContainer targetUserId={id} />
     </PhoneFrame>
   );
 }
